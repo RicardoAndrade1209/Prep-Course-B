85eef034be2ff9fb513501504e312326300e49fc
RicardoAndrade1209 <richard.andrade020813@gmail.com>
2022-11-02 23:51:51 -0500

    Solution 06

--- 06-JS-VI/homework/homework.js
+++ 06-JS-VI/homework/homework.js
@@ -9,12 +9,14 @@ function mayuscula(nombre) {
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +24,16 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var suma = 0;
+  numeros.forEach(elemento => {suma += elemento});
+  return cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+
 }
 
 function map(array, cb) {
@@ -41,6 +47,7 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  array.filter(elemento => {elemento })
 }
 
 // No modificar nada debajo de esta línea
